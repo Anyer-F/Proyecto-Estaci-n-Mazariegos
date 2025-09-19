498da7493b641e0864442b6f3267310e24bdc5bd
Anyer-F <afelixm@miumg.edu.gt>
2025-09-18 22:26:46 -0600

    Proyecto base.

---
+++ src/main/java/com/umg/gastation/GastationApplication.java
@@ -0,0 +1,13 @@
+package com.umg.gastation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastationApplication.class, args);
+	}
+
+}
